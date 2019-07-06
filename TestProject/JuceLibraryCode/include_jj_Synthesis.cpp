32d3d59f6094c58074797b03e9b63f6f1b77aea4
James Johnson <jimyj@hotmail.com>
2019-07-06 20:52:02 +0100

    Initial module setup

---
+++ TestProject/JuceLibraryCode/include_jj_Synthesis.cpp
@@ -0,0 +1,9 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+*/
+
+#include "AppConfig.h"
+#include <jj_Synthesis/jj_Synthesis.cpp>
---
+++ jj_Synthesis.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:               jj_Synthesis
+    vendor:           James Johnson
+    version:          0.1.0
+    name:             jj_Synthesis
+    description:      A simple synthesis module.
+    dependencies:     juce_dsp
+
+    END_JUCE_MODULE_DECLARATION
+*******************************************************************************/
+
+#pragma once
+#define JJ_SYNTHESIS_H_INCLUDED
+
+#include <juce_dsp/juce_dsp.h>
