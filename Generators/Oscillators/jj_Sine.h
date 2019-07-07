a6b9aa173355ddcf0be9df6b17c1f1379b479e73
James Johnson <jimyj@hotmail.com>
2019-07-07 11:38:13 +0100

    Moved Oscillators to new folder

--- Generators/Oscillators/jj_Sine.h
+++ Generators/Oscillators/jj_Sine.h
@@ -8,9 +8,6 @@ namespace jj
     {
     public:
         //======================================================================
-        Sine() {}
-
-        //======================================================================
         float getNextSample() override
         {
             return std::sinf(incrementPhase(true));
